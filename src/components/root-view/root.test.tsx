7caa79d1e8b518e68a0c85cff08b59f36dfba757
Alexander Varney <alex@varney.me>
2022-03-24 21:52:39 -0400

    add user event test utils, first test case

--- src/components/root-view/root.test.tsx
+++ src/components/root-view/root.test.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./root";
+import userEvent from "@testing-library/user-event";
+import RootView from "./root";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("captures keyboard events and updates state as user types", () => {
+  render(<RootView />);
+
+  userEvent.keyboard("test");
+  expect(screen.getByText(/test/i)).toBeInTheDocument();
 });
