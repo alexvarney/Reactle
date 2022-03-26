e6953587ddcb1364469dc280c3b929a2ebb7eaf9
Alexander Varney <alex@varney.me>
2022-03-26 01:18:47 -0400

    shuffle common words

--- src/utils/words-list/find-valid-words.mjs
+++ src/utils/words-list/find-valid-words.mjs
@@ -1,4 +1,5 @@
 import { promises as fs } from "fs";
+import _ from "lodash";
 
 const file = await fs.readFile("./all-words.json", "utf-8");
 
@@ -18,11 +19,18 @@ await fs.writeFile("./valid-words.json", allOutput);
 
 const allFileContents = await fs.readFile("./common-words.txt", "utf-8");
 
-const commonWords = {};
+const commonWords = new Set();
 
 allFileContents.split(/\r?\n/).forEach((line) => {
-  if (line.length === 5) commonWords[line] = 1;
+  if (line.length === 5) commonWords.add(line);
 });
 
-const commonOutput = JSON.stringify(commonWords);
+const shuffled = _.shuffle(Array.from(commonWords));
+
+const objectMap = shuffled.reduce((acc, curr) => {
+  acc[curr] = 1;
+  return acc;
+}, {});
+
+const commonOutput = JSON.stringify(objectMap);
 await fs.writeFile("common-words.json", commonOutput);
