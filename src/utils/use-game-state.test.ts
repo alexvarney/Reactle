d4c89038ce2916768c52cf0a316ff6e631bb9b82
Alexander Varney <alex@varney.me>
2022-04-07 16:49:22 -0400

    todo: useGameState test cases

---
+++ src/utils/use-game-state.test.ts
@@ -0,0 +1,12 @@
+describe("useGameState", () => {
+  it.todo("ignores inputs after the fifth letter of the word");
+  it.todo(
+    "should only accept upper and lowercase letters, ignoring symbols and numbers"
+  );
+  it.todo(
+    "after exactly 5 characters are input, pressing enter locks the guess and jumps to a new line"
+  );
+  it.todo("should only accept valid english words");
+  it.todo("should only accept a word once");
+  it.todo("should allow setting the target word");
+});
